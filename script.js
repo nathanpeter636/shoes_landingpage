6c5df58b06b1d69bd4cf04592b76b56117813e5b
nathanpeter636 <nathanpeter636@gmail.com>
2021-01-28 14:46:48 +1300

    added 3d image and section-two, going to deploy

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //https://easings.net/ easing styles
 
 
+
 $(".toggle").on("click", () => {
   $(".toggle").toggleClass("close");
   $(".toggle__menu").toggleClass("open");
@@ -195,4 +196,41 @@ $(function () {
         opacity: [0, 1],
         delay: anime.stagger(150, {start: 5500})
       });
+});
+
+const card = document.querySelector(".card");
+const container = document.querySelector(".container");
+//Items
+const title = document.querySelector(".title");
+const sneaker = document.querySelector(".sneaker img");
+const circle = document.querySelector(".circle")
+
+
+//Moving Animation Event
+container.addEventListener("mousemove", (e) => {
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 30;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 30;
+  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
+//Animate In
+container.addEventListener("mouseenter", (e) => {
+  card.style.transition = "none";
+  //Popout
+  title.style.transform = "translateZ(100px)";
+
+  sneaker.style.transform = "translateZ(150px) rotateZ(-15deg)";
+  // circle.style.boxShadow="0px 9px 20px 0 #b53f86";
+
+ 
+
+  
+});
+//Animate Out
+container.addEventListener("mouseleave", (e) => {
+  card.style.transition = "all 0.5s ease";
+  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+  //Popback
+  // title.style.transform = "translateZ(0px)";
+  sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
+  
 });
